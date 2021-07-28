a155aed53478b629226415182e7ce38780e9cc2d
ronin-4422 <mmfesenko@gmail.com>
2021-07-28 19:20:07 +0300

    do all tasks

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 
-  
-    
-    let form  = document.getElementById('test');
-    let usernameError = document.querySelector('#username + span.error');
-    let username = document.getElementById('username');
-    let password = document.getElementById('password');
-    
-    form.addEventListener('submit', (event) => { 
-    let regLogin = /^[a-z0-9$#]{2,15}/i;
-    let regPass = /^[a-z0-9$#]{6,15}/i;
-    
-    event.preventDefault();
-    console.log(regPass.test(document.querySelector('#username').value));
-    console.log(regLogin.test(document.querySelector('#password').value));
-    console.log(username.value);
-  })
-  
+let form  = document.getElementById('test');
+let userName = document.getElementById('username');
+let password = document.getElementById('password')
+let btn = document.getElementById('button');
+ 
+form.addEventListener('change', () => { 
+  function validUser() {
+    if ((/^[a-zA-Z0-9]{2,15}/).test(userName.value)) {
+      userName.className = 'valid';
+    } else {
+      userName.className = 'invalid';
+    }
+    console.log(userName.value);
+  }
+  validUser();
+  function validPassword() {
+    if ((/^(?=.*[$#])[a-zA-Z0-9#$]{6,15}/).test(password.value)) {
+      password.className = 'valid';
+    } else {
+      password.className = 'invalid';
+    }
+  }
+  validPassword();
+});
 
+function consoleData(event) {
+  event.preventDefault();
+  if ((/^[a-zA-Z0-9]{2,15}/g).test(userName.value) && 
+  (/^(?=.*[$#])[a-zA-Z0-9#$]{6,15}$/).test(password.value)) {
+    document.forms[0].reset();
+  }
+}
